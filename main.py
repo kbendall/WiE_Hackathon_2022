59035f40b71becc98f30a342a1d755e65015e472
Niveditha <25 niveditha@gmail.com>
2022-11-12 13:46:10 -0500

    Simple response to message function created

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ def main():
     load_dotenv()
     TOKEN = 'MTA0MDk5NzU4OTE4Njc3MzAxMg.GtJT2Z.jPRbLHKXEzs_89MS_Vcgl0mRjg06xaVwdToKAk'
 
-    client = discord.Client(intents=discord.Intents.default())
+    client = discord.Client(intents=discord.Intents.all())
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!!!!!')
+    
+    @client.event
+    async def on_message(message):
+
+        #user information
+        username = str(message.author).split('#')[0]
+        user_message = str(message.content)
+        channel = str(message.channel.name)
+
+        #make sure we're not responding to bot messages
+        if message.author == client.user:
+             return
+
+        #intro welcome message
+        if user_message.lower() == 'wonderwoman':
+            await message.channel.send(f'Hello {username}! Welcome to the Server:) I\'m WonderWoman, nice to meet you!')
 
     client.run(TOKEN)
 
