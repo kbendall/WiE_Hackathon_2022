fcdc398d44edb9618343649e279bfcb82f46cbf3
cassandrah62 <cassandra9hopkins@gmail.com>
2022-11-12 10:13:14 -0500

    Small fix

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = 'MTA0MDk5NzU4OTE4Njc3MzAxMg.GtJT2Z.jPRbLHKXEzs_89MS_Vcgl0mRjg06xaVwdToKAk'
+def main():
+    load_dotenv()
+    TOKEN = 'MTA0MDk5NzU4OTE4Njc3MzAxMg.GtJT2Z.jPRbLHKXEzs_89MS_Vcgl0mRjg06xaVwdToKAk'
 
-client = discord.Client(intents=discord.Intents.default())
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!!!!!')
+    client = discord.Client(intents=discord.Intents.default())
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!!!!!')
 
-client.run(TOKEN)
+    client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
