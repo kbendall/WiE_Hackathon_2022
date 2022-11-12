ab5dd97eb77aec172948bae6d7961b94279ca8d4
cassandrah62 <cassandra9hopkins@gmail.com>
2022-11-12 10:04:34 -0500

    Fixed init bug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = 'MTA0MDk5NzU4OTE4Njc3MzAxMg.GtJT2Z.jPRbLHKXEzs_89MS_Vcgl0mRjg06xaVwdToKAk'
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!!!!!')
